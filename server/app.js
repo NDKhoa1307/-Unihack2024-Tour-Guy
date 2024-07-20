b6fb65f250384cd91e9e4b2e7320314fb1a6fc6e
NDKhoa1307 <khoa130704@gmail.com>
2024-07-20 10:12:27 +0700

    [feat] Added Client & CI/CD Deployment with Docker
    Added a Client folder for Client-side
    Added CI/CD Deployment with Docker

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
-ÿþ
+const express = require('express');
+const app = express()
+
+const port = 8080
+
+app.get('/api/hello', (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`App is live at http://localhost:${port}/api/hello`)
+})
